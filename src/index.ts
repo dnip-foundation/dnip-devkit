8d399a6fde417010b659bc87b82542b59268648f
Volodymyr Sentiurin <volodymyr.sentiurin@gmail.com>
2025-08-28 18:14:03 +0300

    export 'Errors'

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './interfaces/common.interface.js';
 export * as Declarated from './interfaces/declarated.interface.js';
 export * as Implemented from './interfaces/implemented.interface.js';
+export * as Errors from './errors/index.js';
 export { ConfigAdapter } from './config.js';
 export { Runner } from './runner.js';
